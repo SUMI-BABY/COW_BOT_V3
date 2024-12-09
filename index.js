79c7c74dccb2e841cde667df60cc425c7fc03815
🍒)⎯⃝𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 𝐁𝐀𝐘𝐉𝐈𝐃♡༏༏"🩷🐰༉༎🫰🫶 <130752960+BAYJID-404-BOYZ@users.noreply.github.com>
2024-12-09 20:37:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const axios = require("axios");
 const semver = require("semver");
 const logger = require("./utils/log");
 const chalk = require("chalk");
-var uptimelink = [`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`]
+/* var uptimelink = [`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`]
 const Monitor = require('ping-monitor');
 for (const now of uptimelink) {
   const monitor = new Monitor({
     website: `${now}`,
     title: 'ONLINE',
     interval: 59,
-  config: {
-    intervalUnits: 'seconds'
-  }
-});
-monitor.on('up', (res) => console.log(chalk.bold.hex("#00FF00")("[ ONLINE ] ❯ ") + chalk.hex("#00FF00")(`${res.website}`)))
-monitor.on('down', (res) => console.log(chalk.bold.hex("#FF0000")("[ DOWN ] ❯ ") + chalk.hex("#FF0000")(`${res.website} ${res.statusMessage}`)))
-monitor.on('stop', (website) => console.log(chalk.bold.hex("#FF0000")("[ STOP ] ❯ ") + chalk.hex("#FF0000")(`${website}`)))
-monitor.on('error', (error) => console.log(chalk.bold.hex("#FF0000")("[ ERROR ] ❯ ") + chalk.hex("#FF0000")(`${error}`)))
-}
+    config: {
+      intervalUnits: 'seconds'
+    }
+  });
+  monitor.on('up', (res) => console.log(chalk.bold.hex("#00FF00")("[ ONLINE ] ❯ ") + chalk.hex("#00FF00")(`${res.website}`)))
+  monitor.on('down', (res) => console.log(chalk.bold.hex("#FF0000")("[ DOWN ] ❯ ") + chalk.hex("#FF0000")(`${res.website} ${res.statusMessage}`)))
+  monitor.on('stop', (website) => console.log(chalk.bold.hex("#FF0000")("[ STOP ] ❯ ") + chalk.hex("#FF0000")(`${website}`)))
+  monitor.on('error', (error) => console.log(chalk.bold.hex("#FF0000")("[ ERROR ] ❯ ") + chalk.hex("#FF0000")(`${error}`)))
+} */
 /////////////////////////////////////////////
 //========= Check node.js version =========//
 /////////////////////////////////////////////
@@ -36,10 +36,10 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 5000
-     
+
 app.listen(port, () =>
-	logger(`Your app is listening a http://localhost:${port}`, "[ ONLINE ]")
-     );      
+  logger(`Your app is listening a http://localhost:${port}`, "[ ONLINE ]")
+);
 
 
 logger("Opened server site...", "[ Starting ]");
@@ -49,21 +49,21 @@ logger("Opened server site...", "[ Starting ]");
 /////////////////////////////////////////////////////////
 
 function startBot(message) {
-    (message) ? logger(message, "[ Starting ]") : "";
+  (message) ? logger(message, "[ Starting ]"): "";
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "BAYJID.js"], {
-        cwd: __dirname,
-        stdio: "inherit",
-        shell: true
-    });
-  
+  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "BAYJID.js"], {
+    cwd: __dirname,
+    stdio: "inherit",
+    shell: true
+  });
+
   child.on("close", (codeExit) => {
-        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
-            startBot("Starting up...");
-            global.countRestart += 1;
-            return;
-        } else return;
-    });
+    if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
+      startBot("Starting up...");
+      global.countRestart += 1;
+      return;
+    } else return;
+  });
 
   child.on("error", function(error) {
     logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
@@ -81,4 +81,4 @@ axios.get("https://raw.githubusercontent.com/Mrchandu7/chandv2/main/package.json
 });
 startBot();
 // THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
-app.get('/', (req, res) => res.sendFile(__dirname+'/BAYJID.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/BAYJID.html'))
